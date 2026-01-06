cb870bf90e9c424e4702720b7a53b2932990491c
shvgpt-070208 <shvgpt070208@gmail.com>
2026-01-06 18:52:01 +0530

    bug fix

--- src/db/User.js
+++ src/db/User.js
@@ -28,7 +28,7 @@ const UserSchema = new mongoose.Schema({
   status: {
     type: String,
     enum: ["active", "inactive"],
-    default: "inactive",
+    default: "active",
   },
 });
 UserSchema.pre("save", async function () {
